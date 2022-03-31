731c80aa4f6dfc18940940d5dff838c8f2ef70f2
NateCross <jonathan.a.b.cross@gmail.com>
2022-03-31 15:38:52 +0800

    obj URL don't work; switching to arraybuff

--- js/models/BookData.js
+++ js/models/BookData.js
@@ -1,11 +1,12 @@
+// TODO: Find a way to use epub.js functions here so it can
+//       automatically get data
+
 class BookData {
   constructor(bookObjURL, metadata, coverURL = null) {
     this.bookObjURL = bookObjURL;   // An object URL. Used to open with 'ePub(bookURL)'
     this.metadata = metadata;       // Object of metadata. Used to view metadata without opening an ePub
+    this.key = metadata.identifier; // We get this explicitly to use as primary key
     this.coverURL = coverURL;
-    this.key = metadata.identifier;
-  }
-  set metadata() {
-
+    this.currentPage; // TODO: Find a way to get current page/cfi
   }
 }
