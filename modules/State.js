c2a11e81299fc8e199797bd06a41f416d15e53bc
NateCross <jonathan.a.b.cross@gmail.com>
2022-04-28 08:38:28 +0800

    Testing with live server

--- modules/State.js
+++ modules/State.js
@@ -1,10 +1,32 @@
-class State {
+/**
+ * Manages state of the entire app.
+ * Load the epub.js and localforage script before this.
+ * @class
+ * @param {ePub} ePubJS - the epub.js object
+ */
+export default class State {
   constructor(ePubJS) {
     this.ePubJS = ePubJS;
-    this.book;
+
+    /**
+     * Stores the current book.
+     * @private
+     */
+    this._book;
+
+    /**
+     * Stores the current renderer.
+     * @private
+     */
+    this._rendition;
+
+    /**
+     *  bookLib can store either a URL string or an arraybuffer.
+     *  This is going to be stored with localForage.
+     */
     this.bookLib = []; // NOTE: Try to use the localitemstorage here
     this.bookSections = [];
-    this.rendition;
+
     this.currentSection;
     this.currentPage;
     this.totalPages;
@@ -25,10 +47,15 @@ class State {
   // Returns a promise
   // Use in async/await stuff
   // Can be passed to events or used as-is, with a url
-  openBook(e) {
+  /**
+   * Opens a book; can be used as a parameter for a FileReader load event
+   * or used as-is with a link to a file
+   * @param {ArrayBuffer|string}
+   */
+  async openBook(e) {
     let promise = null;
 
-    this.book = this.ePubJS(); // Reset book
+    this._book = this.ePubJS(); // Reset book
 
     let bookData;
 
@@ -40,40 +67,44 @@ class State {
       bookData = e;
 
     try {
-      promise = this.book.open(bookData);
+      promise = this._book.open(bookData);
     } catch (err) {
       console.log(`ERROR: ${err}`);
     }
     return promise;
   }
 
-  storeBookToLib(arrayBuffer) {
-
-  }
+  // storeBookToLib(arrayBuffer) {
+  //
+  // }
 
   renderBook(viewer, width = "100%", height = 600) {
     // this.rendition = null;
     try {
-      this.rendition = this.book.renderTo(viewer, {
+      this._rendition = this._book.renderTo(viewer, {
         width: width,
         height: height,
       });
 
-      this.rendition.display();
+      this._rendition.display();
     } catch (err) {
       console.log(`ERROR: ${err}`);
     }
   }
 
   reset() {
-    if (this.book)
-      this.book.destroy();
-    if (this.rendition)
-      this.rendition.destroy();
+    if (this._book)
+      this._book.destroy();
+    if (this._rendition)
+      this._rendition.destroy();
     // TODO: Insert code to remove inputs
   }
 
   get metadata() {
-    return this.book.packaging.metadata;
+    return this._book.packaging.metadata;
+  }
+
+  get currPage() {
+    return this.currentPage;
   }
 }
--- test/tests-live-server.js
+++ test/tests-live-server.js
@@ -4,15 +4,19 @@
 */
 // Defining global variables so eslint stops nagging me about it
 
+import State from "../modules/State.js";
+
 const assert = chai.assert;
 // const expect = chai.expect;
 
 describe('Load Moby Dick', function() {
   this.timeout(5000);
+  console.log("Test");
 
   it('should load metadata', async function() {
     const state = new State(ePub);
     await state.openBook("https://s3.amazonaws.com/moby-dick/moby-dick.epub");
+    console.log(state.metadata);
     assert.isNotNull(state.metadata);
   });
 });
